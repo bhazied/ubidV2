ab2eaaf1b20448a6799a4e5d6eec4f8bb546ec8f
Sahbi KHALFALLAH <sahbi.khalfallah@continuousnet.com>
2016-12-13 12:02:05 +0100

    add some data

--- database/db_updates_20161211.sql
+++ database/db_updates_20161211.sql
@@ -8,12 +8,23 @@ ALTER TABLE `supplier` CHANGE `email` `email` VARCHAR( 255 ) CHARACTER SET utf8 
 
 ALTER TABLE `tender` ADD `fees` FLOAT NULL AFTER `reference` ;
 
+DELETE FROM `tender_type`;
+
 INSERT INTO `tender_type` (`id`, `name`, `slug`, `is_published`, `created_at`, `creator_user_id`, `modified_at`, `modifier_user_id`) VALUES
 (1, 'Prior Information Notice', 'prior-information-notice', 1, '2016-12-13 00:00:00', 1, '2016-12-13 00:00:00', 1),
 (2, 'Procurement Plan', 'procurement-plan', 1, '2016-12-13 00:00:00', 1, '2016-12-13 00:00:00', 1),
 (3, 'Request For Proposals', 'request-for-froposals', 1, '2016-12-13 00:00:00', 1, '2016-12-13 00:00:00', 1),
 (4, 'Request For Expressions of Interest', 'request-for-expressions-of-interest', 1, '2016-12-13 00:00:00', 1, '2016-12-13 00:00:00', 1),
 (5, 'Prequalification Notice', 'prequalification-notice', 1, '2016-12-13 00:00:00', 1, '2016-12-13 00:00:00', 1),
-(6, 'Contract Awards', 'contract-awards', 1, '2016-12-13 00:00:00', 1, '2016-12-13 00:00:00', 1);
+(6, 'Contract Awards', 'contract-awards', 1, '2016-12-13 00:00:00', 1, '2016-12-13 00:00:00', 1),
+(7, 'Consultants and contractors', 'consultants-and-contractors', 1, '2016-12-13 00:00:00', 1, '2016-12-13 00:00:00', 1),
+(8, 'Goods and works', 'goods-and-works', 1, '2016-12-13 00:00:00', 1, '2016-12-13 00:00:00', 1),
+(9, 'Future procurement', 'future-procurement', 1, '2016-12-13 00:00:00', 1, '2016-12-13 00:00:00', 1);
+
+DELETE FROM `bidding_type`;
+
+INSERT INTO `bidding_type` (`id`, `name`, `slug`, `is_published`, `created_at`, `creator_user_id`, `modified_at`, `modifier_user_id`) VALUES
+(1, 'National or Domestic Competitive Bidding', 'national-or-domestic-competitive-bidding', '1', '2016-12-13 00:00:00', '1', '2016-12-13 00:00:00', '1'),
+(2, 'International Competitive Bidding', 'international-competitive-bidding', '1', '2016-12-13 00:00:00', '1', '2016-12-13 00:00:00', '1');
 
 
