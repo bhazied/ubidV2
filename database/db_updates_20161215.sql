f269dab120f430b447dab7ff33071859b0277135
Sahbi KHALFALLAH <sahbi.khalfallah@continuousnet.com>
2016-12-15 10:47:00 +0100

    before generation

--- database/db_updates_20161215.sql
+++ database/db_updates_20161215.sql
@@ -10,3 +10,9 @@ ALTER TABLE `supplier` CHANGE `total revenu` `total_revenu` INT( 10 ) UNSIGNED N
 ALTER TABLE `supplier` CHANGE `first_market_rate` `first_market_rate` SMALLINT UNSIGNED NULL DEFAULT NULL ,
 CHANGE `second_market_rate` `second_market_rate` SMALLINT UNSIGNED NULL DEFAULT NULL ,
 CHANGE `third_market_rate` `third_market_rate` SMALLINT UNSIGNED NULL DEFAULT NULL ;
+
+ALTER TABLE `buyer` CHANGE `views` `views` INT(11) NULL COMMENT '{"hidden":true}';
+ALTER TABLE `supplier` CHANGE `views` `views` INT(11) NULL COMMENT '{"hidden":true}';
+ALTER TABLE `supplier_product` CHANGE `views` `views` INT(11) NULL COMMENT '{"hidden":true}';
+ALTER TABLE `tender` CHANGE `views` `views` INT(11) NULL COMMENT '{"hidden":true}';
+
