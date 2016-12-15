3cf468b422c8e8d5d1f82f8ce28d502e16c1a387
Sahbi KHALFALLAH <sahbi.khalfallah@continuousnet.com>
2016-12-15 08:13:49 +0100

    after generation

--- src/ContinuousNet/UbidElectricityBundle/Controller/BuyerRESTController.php
+++ src/ContinuousNet/UbidElectricityBundle/Controller/BuyerRESTController.php
@@ -110,6 +110,14 @@ class BuyerRESTController extends BaseRESTController
                    }
                 }
             }
+            $roles = $this->getUser()->getRoles();
+            if (!empty($roles)) {
+                foreach ($roles as $role) {
+                   if (substr_count($role, 'SUB') > 0) {
+                       $qb->andWhere('b_.creatorUser = :creatorUser')->setParameter('creatorUser', $this->getUser()->getId());
+                   }
+                }
+            }
             $qbList = clone $qb;
             $qb->select('count(b_.id)');
             $data['inlineCount'] = $qb->getQuery()->getSingleScalarResult();
@@ -171,6 +179,16 @@ class BuyerRESTController extends BaseRESTController
         try {
             $em = $this->getDoctrine()->getManager();
             $request->setMethod('PATCH'); //Treat all PUTs as PATCH
+            $roles = $this->getUser()->getRoles();
+            if (!empty($roles)) {
+                foreach ($roles as $role) {
+                   if (substr_count($role, 'SUB') > 0) {
+                       if ($entity->getCreatorUser()->getId() != $this->getUser()->getId()) {
+                           return FOSView::create('Not authorized', Codes::HTTP_FORBIDDEN);
+                       }
+                   }
+                }
+            }
             $form = $this->createForm(new BuyerType(), $entity, array('method' => $request->getMethod()));
             $this->removeExtraFields($request, $form);
             $form->handleRequest($request);
@@ -213,6 +231,16 @@ class BuyerRESTController extends BaseRESTController
     public function deleteAction(Request $request, Buyer $entity)
     {
         try {
+            $roles = $this->getUser()->getRoles();
+            if (!empty($roles)) {
+                foreach ($roles as $role) {
+                   if (substr_count($role, 'SUB') > 0) {
+                       if ($entity->getCreatorUser()->getId() != $this->getUser()->getId()) {
+                           return FOSView::create('Not authorized', Codes::HTTP_FORBIDDEN);
+                       }
+                   }
+                }
+            }
             $em = $this->getDoctrine()->getManager();
             $em->remove($entity);
             $em->flush();
--- src/ContinuousNet/UbidElectricityBundle/Controller/TenderProductRESTController.php
+++ src/ContinuousNet/UbidElectricityBundle/Controller/TenderProductRESTController.php
@@ -107,6 +107,14 @@ class TenderProductRESTController extends BaseRESTController
                    }
                 }
             }
+            $roles = $this->getUser()->getRoles();
+            if (!empty($roles)) {
+                foreach ($roles as $role) {
+                   if (substr_count($role, 'SUB') > 0) {
+                       $qb->andWhere('tp_.creatorUser = :creatorUser')->setParameter('creatorUser', $this->getUser()->getId());
+                   }
+                }
+            }
             $qbList = clone $qb;
             $qb->select('count(tp_.id)');
             $data['inlineCount'] = $qb->getQuery()->getSingleScalarResult();
@@ -180,6 +188,16 @@ class TenderProductRESTController extends BaseRESTController
         try {
             $em = $this->getDoctrine()->getManager();
             $request->setMethod('PATCH'); //Treat all PUTs as PATCH
+            $roles = $this->getUser()->getRoles();
+            if (!empty($roles)) {
+                foreach ($roles as $role) {
+                   if (substr_count($role, 'SUB') > 0) {
+                       if ($entity->getCreatorUser()->getId() != $this->getUser()->getId()) {
+                           return FOSView::create('Not authorized', Codes::HTTP_FORBIDDEN);
+                       }
+                   }
+                }
+            }
             $form = $this->createForm(new TenderProductType(), $entity, array('method' => $request->getMethod()));
             $this->removeExtraFields($request, $form);
             $form->handleRequest($request);
@@ -231,6 +249,16 @@ class TenderProductRESTController extends BaseRESTController
     public function deleteAction(Request $request, TenderProduct $entity)
     {
         try {
+            $roles = $this->getUser()->getRoles();
+            if (!empty($roles)) {
+                foreach ($roles as $role) {
+                   if (substr_count($role, 'SUB') > 0) {
+                       if ($entity->getCreatorUser()->getId() != $this->getUser()->getId()) {
+                           return FOSView::create('Not authorized', Codes::HTTP_FORBIDDEN);
+                       }
+                   }
+                }
+            }
             $em = $this->getDoctrine()->getManager();
             $em->remove($entity);
             $em->flush();
