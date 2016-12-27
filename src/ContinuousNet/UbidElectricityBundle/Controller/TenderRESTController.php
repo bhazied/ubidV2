208b37a576a006dcd54b131bbdc2b27ec1793d73
Sahbi KHALFALLAH <sahbi.khalfallah@continuousnet.com>
2016-12-27 15:30:53 +0100

    after generation

--- src/ContinuousNet/UbidElectricityBundle/Controller/TenderRESTController.php
+++ src/ContinuousNet/UbidElectricityBundle/Controller/TenderRESTController.php
@@ -91,7 +91,7 @@ class TenderRESTController extends BaseRESTController
             $qb->leftJoin('ContinuousNet\UbidElectricityBundle\Entity\BiddingType', 'bidding_type', \Doctrine\ORM\Query\Expr\Join::WITH, 't_.biddingType = bidding_type.id');
             $qb->leftJoin('ContinuousNet\UbidElectricityBundle\Entity\User', 'creator_user', \Doctrine\ORM\Query\Expr\Join::WITH, 't_.creatorUser = creator_user.id');
             $qb->leftJoin('ContinuousNet\UbidElectricityBundle\Entity\User', 'modifier_user', \Doctrine\ORM\Query\Expr\Join::WITH, 't_.modifierUser = modifier_user.id');
-            $textFields = array('tender.title', 'tender.slug', 'tender.reference', 'tender.description', 'tender.address', 'tender.email', 'tender.phone', 'tender.attachmentFile1', 'tender.attachmentFile2', 'tender.attachmentFile3', 'tender.attachmentFile4', 'tender.source');
+            $textFields = array('tender.title', 'tender.slug', 'tender.reference', 'tender.description', 'tender.address', 'tender.email', 'tender.phone', 'tender.attachmentFiles', 'tender.source');
             foreach ($filters as $field => $value) {
                 if (substr_count($field, '.') > 1) {
                     if ($value == 'true') {
