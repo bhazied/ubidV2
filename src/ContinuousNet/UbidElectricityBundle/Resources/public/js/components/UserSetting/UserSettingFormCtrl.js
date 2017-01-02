e2e4f69b28f52fdb165cdad6ccc627446daa920a
Sahbi KHALFALLAH <sahbi.khalfallah@continuousnet.com>
2017-01-02 21:01:00 +0100

    after generation

--- src/ContinuousNet/UbidElectricityBundle/Resources/public/js/components/UserSetting/UserSettingFormCtrl.js
+++ src/ContinuousNet/UbidElectricityBundle/Resources/public/js/components/UserSetting/UserSettingFormCtrl.js
@@ -10,6 +10,7 @@ function($scope, $state, $stateParams, $sce, $timeout, $filter, $uibModal, $q, $
     $scope.locale = (angular.isDefined($localStorage.language))?$localStorage.language:'en';
 
     $scope.disableSubmit = false;
+    $scope.enableFormAlert = true;
 
     // Editor options.
     $scope.editorOptions = {
@@ -65,7 +66,9 @@ function($scope, $state, $stateParams, $sce, $timeout, $filter, $uibModal, $q, $
                 }
             }
             angular.element('.ng-invalid[name=' + firstError + ']').focus();
-            SweetAlert.swal($filter('translate')('content.form.messages.FORMCANNOTBESUBMITTED'), $filter('translate')('content.form.messages.ERRORSAREMARKED'), "error");
+            if ($scope.enableFormAlert) {
+                SweetAlert.swal($filter('translate')('content.form.messages.FORMCANNOTBESUBMITTED'), $filter('translate')('content.form.messages.ERRORSAREMARKED'), "error");
+            }
             return false;
         } else {
             if ($scope.userSetting.id > 0) {
