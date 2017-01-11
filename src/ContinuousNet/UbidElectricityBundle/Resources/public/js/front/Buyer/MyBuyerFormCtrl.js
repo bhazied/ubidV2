ca7d6fc1c82c2a4d0c666203838f4346e824d50a
Zied Ben Hadj Amor <zied.benhadjamor@continuousnet.com>
2017-01-11 11:00:07 +0100

    add buyer 2 step

--- src/ContinuousNet/UbidElectricityBundle/Resources/public/js/front/Buyer/MyBuyerFormCtrl.js
+++ src/ContinuousNet/UbidElectricityBundle/Resources/public/js/front/Buyer/MyBuyerFormCtrl.js
@@ -18,11 +18,58 @@ function($scope, $controller, $rootScope, $state, $stateParams, $sce, $timeout, 
 
     angular.extend(this, $controller('BuyerFormCtrl', {$scope:$scope}));
 
+    $scope.steps = [
+        {title : $filter('translate')('front.ADDBUYERSTEP1'), description: $filter('translate')('front.DESCRIPTIONSTEP1'),  id: 1},
+        {title : $filter('translate')('front.ADDBUYERSTEP2'), description: $filter('translate')('front.DESCRIPTIONSTEP2'), id: 2}
+    ];
+
+    $scope.currentStep = 1;
+    $scope.isNext = false;
     $scope.enableFormAlert = false;
 
     $scope.list = function() {
         $state.go('front.mybuyers.list');
     };
+    
+    $scope.goNext = function (form) {
+        $scope.toTheTop();
+        if(form.$valid){
+            form.$setPristine();
+            $scope.currentStep++;
+        }
+        else{
+            var field = null, firstError = null;
+            for (field in form) {
+                if (field[0] != '$') {
+                    if (firstError === null && !form[field].$valid) {
+                        firstError = form[field].$name;
+                    }
+
+                    if (form[field].$pristine) {
+                        form[field].$dirty = true;
+                    }
+                }
+            }
+
+            angular.element('.ng-invalid[name=' + firstError + ']').focus();
+            $scope.isNext = true;
+        }
+    }
+    
+    $scope.goPrevious = function () {
+        $scope.toTheTop();
+        $scope.currentStep--;
+    }
+    
+    $scope.goto = function (step, form) {
+        if(step == 1){
+            $scope.goNext(form);
+        }else{
+            $scope.goPrevious();
+        }
+    }
+
+    
 
 }]);
 
