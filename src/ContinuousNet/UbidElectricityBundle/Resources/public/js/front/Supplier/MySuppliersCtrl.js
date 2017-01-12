0fa8bf32bc29a9231be9c4bedbee4847f568c9e1
Zied Ben Hadj Amor <zied.benhadjamor@continuousnet.com>
2017-01-12 12:00:10 +0100

    update all list of teh site buyer, supplier, tender ...

--- src/ContinuousNet/UbidElectricityBundle/Resources/public/js/front/Supplier/MySuppliersCtrl.js
+++ src/ContinuousNet/UbidElectricityBundle/Resources/public/js/front/Supplier/MySuppliersCtrl.js
@@ -18,6 +18,27 @@ function($scope, $controller, $rootScope, $stateParams, $location, $sce, $timeou
 
     angular.extend(this, $controller('SuppliersCtrl', {$scope:$scope}));
 
+
+    $scope.setCols = function() {
+        $scope.cols = [
+            { field: 'picture', title: $filter('translate')('content.list.fields.PICTURE'), sortable: 'supplier.picture', filter: { 'supplier.picture': 'text' }, show: true, getValue: $scope.interpolatedValue, interpolateExpr: $interpolate('<img ng-src="'+$rootScope.app.thumbURL+'[[ (row.picture)?row.picture:\'/assets/images/picturenotavailable.'+$scope.locale+'.png\' ]]" alt="" class="img-thumbnail" />') },
+            { field: 'name', title: $filter('translate')('content.list.fields.NAME'), sortable: 'supplier.name', filter: { 'supplier.name': 'text' }, show: $scope.getParamValue('name_show_filed', true), getValue: $scope.textValue },
+            { field: 'supplier_type', title: $filter('translate')('content.list.fields.SUPPLIERTYPE'), sortable: 'supplier_type.name', filter: { 'supplier.supplierType': 'select' }, getValue: $scope.linkValue, filterData: $scope.getSupplierTypes(), show: true, displayField: 'name', state: 'app.lists.suppliertypesdetails' },
+            { field: 'company_name', title: $filter('translate')('content.list.fields.COMPANYNAME'), sortable: 'supplier.companyName', filter: { 'supplier.companyName': 'text' }, show: true, getValue: $scope.textValue },
+            { field: 'first_name', title: $filter('translate')('content.list.fields.FIRSTNAME'), sortable: 'supplier.firstName', filter: { 'supplier.firstName': 'text' }, show: true, getValue: $scope.textValue },
+            { field: 'last_name', title: $filter('translate')('content.list.fields.LASTNAME'), sortable: 'supplier.lastName', filter: { 'supplier.lastName': 'text' }, show: true, getValue: $scope.textValue },
+            { title: $filter('translate')('content.common.ACTIONS'), show: true, getValue: $scope.interpolatedValue, interpolateExpr: $interpolate(''
+                +'<div class="btn-group pull-right">'
+                +'<button type="button" class="btn btn-success" tooltip-placement="top" uib-tooltip="'+$filter('translate')('content.common.EDIT')+'" ng-click="edit(row)"><i class="ti-pencil-alt"></i></button>'
+                +'<button type="button" class="btn btn-warning" tooltip-placement="top" uib-tooltip="'+$filter('translate')('content.common.SHOWDETAILS')+'" ng-click="details(row)"><i class="ti-clipboard"></i></button>'
+                +'<button type="button" class="btn btn-danger" tooltip-placement="top" uib-tooltip="'+$filter('translate')('content.common.REMOVE')+'" ng-click="delete(row)"><i class="ti-trash"></i></button>'
+                +'</div>') }
+        ];
+    };
+
+    $scope.setCols();
+
+
     $scope.add = function() {
         $state.go('front.mysuppliers.new');
     };
--- src/ContinuousNet/UbidElectricityBundle/Resources/public/js/front/Tender/MyTendersCtrl.js
+++ src/ContinuousNet/UbidElectricityBundle/Resources/public/js/front/Tender/MyTendersCtrl.js
@@ -18,6 +18,25 @@ function($scope, $controller, $rootScope, $stateParams, $location, $sce, $timeou
 
     angular.extend(this, $controller('TendersCtrl', {$scope:$scope}));
 
+    $scope.setCols = function() {
+        $scope.cols = [
+            { field: 'buyer', title: $filter('translate')('content.list.fields.BUYER'), sortable: 'buyer.name', filter: { 'tender.buyer': 'select' }, getValue: $scope.linkValue, filterData: $scope.getBuyers(), show: true, displayField: 'name', state: 'app.marketplace.buyersdetails' },
+            { field: 'reference', title: $filter('translate')('content.list.fields.REFERENCE'), sortable: 'tender.reference', filter: { 'tender.reference': 'text' }, show: true, getValue: $scope.textValue },
+            { field: 'tender_type', title: $filter('translate')('content.list.fields.TENDERTYPE'), sortable: 'tender_type.name', filter: { 'tender.tenderType': 'select' }, getValue: $scope.linkValue, filterData: $scope.getTenderTypes(), show: true, displayField: 'name', state: 'app.lists.tendertypesdetails' },
+            { field: 'title', title: $filter('translate')('content.list.fields.TITLE'), sortable: 'tender.title', filter: { 'tender.title': 'text' }, show: true, getValue: $scope.textValue },
+            { field: 'bidding_type', title: $filter('translate')('content.list.fields.BIDDINGTYPE'), sortable: 'bidding_type.name', filter: { 'tender.biddingType': 'select' }, getValue: $scope.linkValue, filterData: $scope.getBiddingTypes(), show: true, displayField: 'name', state: 'app.lists.biddingtypesdetails' },
+            { title: $filter('translate')('content.common.ACTIONS'), show: true, getValue: $scope.interpolatedValue, interpolateExpr: $interpolate(''
+                +'<div class="btn-group pull-right">'
+                +'<button type="button" class="btn btn-success" tooltip-placement="top" uib-tooltip="'+$filter('translate')('content.common.EDIT')+'" ng-click="edit(row)"><i class="ti-pencil-alt"></i></button>'
+                +'<button type="button" class="btn btn-warning" tooltip-placement="top" uib-tooltip="'+$filter('translate')('content.common.SHOWDETAILS')+'" ng-click="details(row)"><i class="ti-clipboard"></i></button>'
+                +'<button type="button" class="btn btn-danger" tooltip-placement="top" uib-tooltip="'+$filter('translate')('content.common.REMOVE')+'" ng-click="delete(row)"><i class="ti-trash"></i></button>'
+                +'</div>') }
+        ];
+    };
+
+    $scope.setCols();
+
+
     $scope.add = function() {
         $state.go('front.mytenders.new');
     };
