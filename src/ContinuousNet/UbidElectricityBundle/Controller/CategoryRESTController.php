2184220ac6edfb8253c32b9bb89c029bf09fda4a
Sahbi KHALFALLAH <sahbi.khalfallah@continuousnet.com>
2017-02-03 21:05:07 +0100

    after generation

--- src/ContinuousNet/UbidElectricityBundle/Controller/CategoryRESTController.php
+++ src/ContinuousNet/UbidElectricityBundle/Controller/CategoryRESTController.php
@@ -88,7 +88,7 @@ class CategoryRESTController extends BaseRESTController
             $qb->leftJoin('ContinuousNet\UbidElectricityBundle\Entity\ProductType', 'product_type', \Doctrine\ORM\Query\Expr\Join::WITH, 'c_.productType = product_type.id');
             $qb->leftJoin('ContinuousNet\UbidElectricityBundle\Entity\User', 'creator_user', \Doctrine\ORM\Query\Expr\Join::WITH, 'c_.creatorUser = creator_user.id');
             $qb->leftJoin('ContinuousNet\UbidElectricityBundle\Entity\User', 'modifier_user', \Doctrine\ORM\Query\Expr\Join::WITH, 'c_.modifierUser = modifier_user.id');
-            $textFields = array('category.name', 'category.slug', 'category.picture', 'category.description');
+            $textFields = array('category.name', 'category.slug', 'category.picture', 'category.description', 'category.metaTitle', 'category.metaDescription', 'category.metaKeywords');
             foreach ($filters as $field => $value) {
                 if (substr_count($field, '.') > 1) {
                     if ($value == 'true') {
