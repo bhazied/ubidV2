b1dc02e59c259d2fc47bbeb0faecefa5362a586b
Zied Ben Hadj Amor <zied.benhadjamor@continuousnet.com>
2017-03-01 19:01:57 +0100

    select country default label

--- src/ContinuousNet/UbidElectricityBundle/Resources/public/js/front/Buyer/MyBuyerFormCtrl.js
+++ src/ContinuousNet/UbidElectricityBundle/Resources/public/js/front/Buyer/MyBuyerFormCtrl.js
@@ -137,5 +137,15 @@ function($scope, $controller, $rootScope, $state, $stateParams, $sce, $timeout, 
         }
     ];
 
+   /* $scope.$watch('countries', function () {
+        if($scope.countries.length > 0){
+            var defaultCountry = {id: '', name: $filter('translate')('content.form.messages.SELECTCOUNTRY')};
+            $scope.countries.unshift(defaultCountry);
+            $scope.buyer.country = defaultCountry.id;
+        }
+
+
+    });*/
+
 }]);
 
