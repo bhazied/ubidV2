9f4c2884a2a1acd73e90760c64a0a4244391e5f3
Sahbi KHALFALLAH <sahbi.khalfallah@continuousnet.com>
2017-04-03 09:42:29 +0100

    fix filter

--- web/js/5268854.js
+++ web/js/5268854.js
@@ -67690,7 +67690,8 @@ app.run(['$rootScope', '$state', '$stateParams', '$localStorage', '$timeout',
                 state.indexOf('supplierproduct') > -1 ||
                 state.indexOf('supplier') > -1 ||
                 state.indexOf('buyer') > -1 ||
-                state.indexOf('tender') > -1
+                state.indexOf('tender') > -1 ||
+                state.indexOf('bid') > -1
             ) {
                 return true;
             } else {
@@ -72488,46 +72489,47 @@ function ($rootScope, ToggleHelper) {
  * Check if field is unique or not
  */
 app.directive('myUniqueField', ['$resource', '$rootScope', '$localStorage',
-function($resource, $rootScope, $localStorage) {
-    var timeoutId;
-    return {
-        restrict: 'A',
-        require: 'ngModel',
-        link: function(scope, elem, attrs, ctrl) {
-            //when the scope changes, check the email.
-            scope.$watch(attrs.ngModel, function(value) {
-                // if there was a previous attempt, stop it.
-                if (timeoutId) {
-                    clearTimeout(timeoutId);
-                }
-                // start a new attempt with a delay to keep it from
-                // getting too "chatty".
-                timeoutId = setTimeout(function() {
-                    var fieldName = attrs.myUniqueField;
-                    var resourceURL = attrs.myResourceUrl;
-                    var currentId = attrs.myCurrentId;
-                    var resource = $resource('/' + $localStorage.language + $rootScope.app.apiURL + resourceURL, {id: '@id'}, {
-                        query: { method: 'GET' }
-                    });
-                    var http_params = {
-                        offset: 0,
-                        limit: 1
-                    };
-                    http_params['filters['+fieldName+']'] = value;
-                    resource.query(http_params).$promise.then(function(data) {
-                        var count = data.inlineCount;
-                        if (count > 0) {
-                            if (data.results[0].id == currentId) {
-                                count--;
+    function($resource, $rootScope, $localStorage) {
+        var timeoutId;
+        return {
+            restrict: 'A',
+            require: 'ngModel',
+            link: function(scope, elem, attrs, ctrl) {
+                //when the scope changes, check the email.
+                scope.$watch(attrs.ngModel, function(value) {
+                    // if there was a previous attempt, stop it.
+                    if (timeoutId) {
+                        clearTimeout(timeoutId);
+                    }
+                    // start a new attempt with a delay to keep it from
+                    // getting too "chatty".
+                    timeoutId = setTimeout(function() {
+                        var fieldName = attrs.myUniqueField;
+                        var resourceURL = attrs.myResourceUrl;
+                        var currentId = attrs.myCurrentId;
+                        var resource = $resource('/' + $localStorage.language + $rootScope.app.apiURL + resourceURL, {id: '@id'}, {
+                            query: { method: 'GET' }
+                        });
+                        var http_params = {
+                            offset: 0,
+                            limit: 1
+                        };
+                        http_params['filters['+fieldName+']'] = value;
+                        http_params['filter_operator'] = 'eq';
+                        resource.query(http_params).$promise.then(function(data) {
+                            var count = data.inlineCount;
+                            if (count > 0) {
+                                if (data.results[0].id == currentId) {
+                                    count--;
+                                }
                             }
-                        }
-                        ctrl.$setValidity('myUniqueField', (count == 0));
-                    });
-                }, 500);
-            });
+                            ctrl.$setValidity('myUniqueField', (count == 0));
+                        });
+                    }, 500);
+                });
+            }
         }
-    }
-}]);
+    }]);
 
 'use strict';
 
