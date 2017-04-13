8397f4037c08f01bbec7a62ea9e64fcbb3eb8f42
Sahbi KHALFALLAH <sahbi.khalfallah@continuousnet.com>
2017-04-13 21:30:36 +0100

    fix urls

--- src/ContinuousNet/UbidElectricityBundle/Resources/public/js/front/Main/FrontCtrl.js
+++ src/ContinuousNet/UbidElectricityBundle/Resources/public/js/front/Main/FrontCtrl.js
@@ -39,7 +39,6 @@ app.controller('FrontCtrl', ['$rootScope', '$scope', '$state', '$translate', '$l
             'front.buyers',
             'front.buyer',
             'front.suppliers',
-            'front.supplierscategory',
             'front.supplier',
             'front.post',
             'front.contact',
@@ -138,17 +137,7 @@ app.controller('FrontCtrl', ['$rootScope', '$scope', '$state', '$translate', '$l
                     $rootScope.contentOffset = 0;
                 })
             }
-            if( $state.current.name.indexOf('front.mybuyers') != -1 ){
-                $timeout(function() {
-                    $rootScope.showSlogan = false;
-                    $rootScope.showLeftSide = false;
-                    $rootScope.showRightSide = false;
-                    $rootScope.showUserMenu = true;
-                    $rootScope.contentSize = 10;
-                    $rootScope.contentOffset = 0;
-                }, 2000);
-            }
-            if($state.current.name == 'front.login'){
+            if ($state.current.name == 'front.login') {
                 $timeout(function() {
                     $rootScope.showSlogan = false;
                     $rootScope.showLeftSide = false;
@@ -158,7 +147,7 @@ app.controller('FrontCtrl', ['$rootScope', '$scope', '$state', '$translate', '$l
                     $rootScope.contentOffset = 3;
                 }, 1500);
             }
-            if($state.current.name == 'front.generic_search'){
+            if ($state.current.name == 'front.generic_search') {
                 $timeout(function() {
                     $rootScope.showSlogan = false;
                     $rootScope.showLeftSide = true;
@@ -321,12 +310,6 @@ app.controller('FrontCtrl', ['$rootScope', '$scope', '$state', '$translate', '$l
             }
         });
 
-        $scope.add_tender = function () {
-            $state.go('front.tender.add');
-        }
-
-        $scope.show_tender = function (id) {
-            $state.go('front.tender', {id: id})
-        }
         
-    }]);
+    }
+]);
